6ae64891312feaa71e47efbce5267df2b504bfc2
Brendan Ragan <lordmortis@gmail.com>
2024-02-12 16:12:04 +0800

    add tooltips for showing the value

--- src/components/BarGraph.tsx
+++ src/components/BarGraph.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, RefObject} from "react";
+import React, {useEffect, useRef} from "react";
 import * as d3 from "d3";
 import { Record } from "../reducers/App";
 
@@ -26,19 +26,21 @@ function convertRecordTime(record:Record): string {
 }
 
 export default function BarGraph(props:IProps) : React.ReactElement {
-  const ref : RefObject<SVGGElement | undefined> = useRef();
+  const svgRef = useRef<SVGSVGElement>(null);
+  const toolTipRef = useRef<HTMLDivElement>(null);
 
   const margin = {top: 20, right: 20, bottom: 30, left: 40};
-  const width = (ref.current?.clientWidth === undefined ? 100 : ref.current?.clientWidth) - margin.left - margin.right;
-  const height = (ref.current?.clientHeight === undefined ? 100 : ref.current?.clientHeight) - margin.top - margin.bottom;
-
+  const width = (svgRef.current?.clientWidth === undefined ? 100 : svgRef.current?.clientWidth) - margin.left - margin.right;
+  const height = (svgRef.current?.clientHeight === undefined ? 100 : svgRef.current?.clientHeight) - margin.top - margin.bottom;
 
   const x = d3.scaleBand().range([0, width]).padding(0.1);
   const y = d3.scaleLinear().range([height, 0]);
 
   useEffect(() => {
-    if (ref.current === undefined) return;
-    const svgElement = d3.select(ref.current);
+    if (svgRef.current === undefined) return;
+    if (toolTipRef.current === null) return;
+    const toolTipElement = d3.select(toolTipRef.current);
+    const svgElement = d3.select(svgRef.current);
     if (svgElement.node() == null) return;
     //@ts-ignore
     let gElem = findOrAppend(svgElement, "g", "graph");
@@ -58,6 +60,19 @@ export default function BarGraph(props:IProps) : React.ReactElement {
             .attr("y", (elem) => y(elem.kWh))
             .attr("width", x.bandwidth)
             .attr("height", (elem) => height - y(elem.kWh))
+            .on('mouseover',
+              function (d, record) {
+                d3.select(this).transition().duration(50).attr('opacity', '.5');
+                toolTipElement.style('opacity', 1)
+                  .style('left', `${d.clientX + 10}px`)
+                  .style('top', `${d.clientY + 10}px`)
+                  .text(`${record.kWh} kWh - ${convertRecordTime(record)}`)
+            })
+            .on('mouseout',
+              function(d, record) {
+                d3.select(this).transition().duration(50).attr('opacity', '1')
+                toolTipElement.style('opacity', 0);
+              })
         , update =>
           update.transition().duration(750)
             .attr("x", (elem):number => x(convertRecordTime(elem)) !== undefined ? x(convertRecordTime(elem)) as number : 0)
@@ -80,7 +95,13 @@ export default function BarGraph(props:IProps) : React.ReactElement {
 
   }, [props.records, props.maxPower, margin.left, margin.top, width, height, x, y]);
 
-  //@ts-ignore
-  return <svg ref={ref} preserveAspectRatio="xMidYMid meet"></svg>;
+
+  // @ts-ignore
+  return (
+      <div id="graph">
+        <svg ref={svgRef} preserveAspectRatio="xMidYMid meet"></svg>
+        <div ref={toolTipRef} id="tooltip"></div>
+      </div>
+  );
 }
 
